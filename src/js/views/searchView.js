045d492ba41488dfd0008161a86ad9d34d1b11e8
adityabharti <aditya.9395@gmail.com>
2020-07-03 15:36:02 +0530

    Truncate long recipe titles

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,6 +2,30 @@ import { elements } from './base';
 
 export const getInput = () =>  elements.searchInput.value;
 
+export const clearInput = () => {
+    elements.searchInput.value = '';
+};
+
+export const clearResults = () => {
+    elements.searchResList.innerHTML = '';
+};
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+
+    if (title.length > limit){
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit){
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -10,16 +34,16 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
     `
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
-}
+};
 
 export const renderResults = recipes => {
     console.log(recipes);
     recipes.forEach(renderRecipe);
-}
+};
