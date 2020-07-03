2b78eefad6f93313e5255f93ee1c3cebb70575ab
adityabharti <aditya.9395@gmail.com>
2020-07-03 14:59:26 +0530

    Build the search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,36 @@
 // Global app controller
-import axios from 'axios';
 import Search from './models/Search';
+import * as searchView  from './views/searchView';
+import { elements } from './views/base';
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Liked recipes
+ */
+const state = {};
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
+
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log("hahaha" + state.search.result);
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,25 @@
-export const add = (a, b) => a + b;
-export const multiply = (a, b) => a * b;
-export const ID = 23;
+import { elements } from './base';
+
+export const getInput = () =>  elements.searchInput.value;
+
+const renderRecipe = recipe => {
+    const markup = `
+        <li>
+            <a class="results__link" href="#${recipe.recipe_id}">
+                <figure class="results__fig">
+                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${recipe.title}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
+                </div>
+            </a>
+        </li>
+    `
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+}
+
+export const renderResults = recipes => {
+    console.log(recipes);
+    recipes.forEach(renderRecipe);
+}
